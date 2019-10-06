6691e9aa6fe2eea5ba3ac082ef3cbf523153453e
6ry0u <6ry0u@tutanota.com>
2019-10-06 22:23:28 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const sum = require('./sum')
+
+const total = sum(10, 2)
+console.log(total)
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // ends with .js
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+
+}
+
+module.exports = config
