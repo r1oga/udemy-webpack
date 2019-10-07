c7bd6d509fbb5348dea78d2bd8786875a497c840
Gauthier Riou <gauthier.riou@airbus.com>
2019-10-07 11:40:01 +0200

    Try code splitting

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,12 +1,8 @@
 import '../styles/image_viewer.css'
-import big from '../assets/big.jpeg' // needs to resolve right path
 import small from '../assets/small.jpeg' // base64 string
 
-const image = document.createElement('img')
-image.src = small
-
-document.body.appendChild(image)
-const bigImage = document.createElement('img')
-bigImage.src = big
-
-document.body.appendChild(bigImage)
+export default () => {
+  const image = document.createElement('img')
+  image.src = small
+  document.body.appendChild(small)
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import sum from './sum'
-import './image_viewer'
+const button = document.createElement('button')
+button.innerText = 'Click me'
+button.onclick = () => {
+  // System global var
+  // Returns promise
+  System
+    .import('./image_viewer')
+    .then(module => {
+      module.default()
+    })
+}
 
-const total = sum(10, 2)
-console.log(total)
+document.body.appendChild(button)
